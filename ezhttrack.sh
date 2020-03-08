607235ae541174c265b55869d9c73d49d6f996a8
Peter Wroot <p.wroot@icloud.com>
2020-03-08 11:46:30 +0000

    added ezhttrack.sh

---
+++ ezhttrack.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# ezhhtrack - a simple bash script for automating single-page httrack downloads
+# I mostly use httrack to archive interesting webpages for later reading - this
+# script will download only a single page, but will download all external 
+# resources (such as images) required to make the page displat identically to
+# the online version.
+
+# REQUIREMENTS:
+# httrack (brew install httrack)
+# recode (brew install recode)
+
+# get title of webpage and format it (for folder name)
+input=`curl -sL "$1" | grep '<title>' | recode html..ascii`
+articleTitle=`echo $input | sed 's/<title>//g' | sed 's#</title>##g' | sed -e "s/[^A-Za-z0-9._' -]/ /g"`
+
+homeDirectory=`printenv HOME`
+fullSavePath="${homeDirectory}/websites/${articleTitle}"
+
+httrack "$1" -O "${fullSavePath}" "$1" -v -s0 --ext-depth=0 --depth=1 -n
