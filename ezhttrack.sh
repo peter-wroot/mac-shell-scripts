fadedd6d0564ffcac6afcf291048b368249e8910
Peter Wroot <p.wroot@icloud.com>
2020-03-09 21:28:11 +0000

    removed automatic titling

--- ezhttrack.sh
+++ ezhttrack.sh
@@ -11,13 +11,12 @@
 # recode (brew install recode)
 
 # USAGE:
-# ./ezhttrack.sh https://your-webpage-url-here.com/article/blah/blah/blah
+# ./ezhttrack.sh https://your-webpage-url-here.com/article/blah/blah/blah "Article Title Here"
 
 # get title of webpage and format it (for folder name)
-input=`curl -sL "$1" | grep '<title>' | recode html..ascii`
-articleTitle=`echo $input | sed 's/<title>//g' | sed 's#</title>##g' | sed -e "s/[^A-Za-z0-9._' -]/ /g"`
+articleTitle=`echo $2 | sed -e "s/[^A-Za-z0-9._' -]/ /g"`
 
 homeDirectory=`printenv HOME`
 fullSavePath="${homeDirectory}/websites/${articleTitle}"
 
-httrack "$1" -O "${fullSavePath}" "$1" -v -s0 --ext-depth=0 --depth=1 -n
+httrack "$1" -O "${fullSavePath}" "$1" -v -s0 --ext-depth=1 --depth=1 -n
